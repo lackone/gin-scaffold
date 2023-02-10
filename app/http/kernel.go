10a743622b5663f6e96916170dd132f59befcbdf
lackone <lackone@126.com>
2023-02-10 17:02:38 +0800

    修改

--- app/http/kernel.go
+++ app/http/kernel.go
@@ -1,11 +1,14 @@
 package http
 
 import (
+	_ "github.com/lackone/gin-scaffold/docs/swagger" //千万别忘了导入生成的docs
 	"github.com/lackone/gin-scaffold/internal/contract"
 	"github.com/lackone/gin-scaffold/internal/core"
 	"github.com/lackone/gin-scaffold/internal/global"
 	"github.com/lackone/gin-scaffold/internal/middleware"
 	"github.com/lackone/gin-scaffold/routes"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"net/http"
 )
 
@@ -18,6 +21,12 @@ func NewHttpEngine(container core.IContainer) (*core.Engine, error) {
 	global.Engine.Use(middleware.Translations())
 
 	configService := container.MustMake(contract.ConfigKey).(contract.Config)
+
+	// 如果配置了swagger，则显示swagger的中间件
+	if configService.GetBool("app.swagger") == true {
+		global.Engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
+
 	global.Engine.StaticFS("/uploads", http.Dir(configService.GetString("app.upload_save_path")))
 
 	routes.ApiRoutes(global.Engine)
